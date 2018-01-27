b4e93e9f94e2fc849af8984b0928afbb31885b1c
awesomealvin <fookinhellvin@outlook.com>
2018-01-27 19:33:51 +1300

    Calculates max length percentage of draw

---
+++ Assets/Scripts/Ballista.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballista : MonoBehaviour {
+
+	[SerializeField]
+	private Transform flightGroove;
+
+	[SerializeField]
+	private Transform shootPoint;
+
+	private bool isAiming;
+
+	[SerializeField]
+	private Queue<GameObject> arrows;
+
+	private Rigidbody2D rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		arrows = new Queue<GameObject>();
+	}
+
+
+	public void Aim(Vector2 firstPos, Vector2 secondPos) {
+		Vector2 _difference = firstPos - secondPos;
+
+		// Sets the initial click rotation
+		if (firstPos.Equals(secondPos)) {
+			_difference = firstPos - (Vector2)flightGroove.transform.position;
+		}
+
+		float _angle = Mathf.Atan2(_difference.y, _difference.x) * Mathf.Rad2Deg;
+		
+		flightGroove.rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
+	}
+
+	public void Shoot() {
+
+	}
+}
---
+++ Assets/Scripts/BallistaController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Ballista))]
+public class BallistaController : MonoBehaviour {
+
+	[SerializeField]
+	private Ballista ballista;
+
+	[SerializeField]
+	private float maxDrawLength;
+	private float currentDrawLength;
+
+	[SerializeField]
+	private Text lengthPercentageText;
+
+	private bool isMouseDown;
+
+	private Vector2 initialTouchPos;
+	private Vector2 currentTouchPos;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+		if (Input.GetMouseButtonDown(0)) {
+			isMouseDown = true;
+			initialTouchPos = GetCurrentMousePos();
+			
+			
+		}
+
+		if (isMouseDown) {
+			currentTouchPos = GetCurrentMousePos();
+			ballista.Aim(initialTouchPos, currentTouchPos);
+			CalculateLength();
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+			isMouseDown = false;
+		}
+	}
+
+	private Vector2 GetCurrentMousePos() {
+		return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	private void CalculateLength() {
+		currentDrawLength = (currentTouchPos - initialTouchPos).magnitude;
+		currentDrawLength = (currentDrawLength > maxDrawLength)?maxDrawLength:currentDrawLength;
+		float _percentage = currentDrawLength / maxDrawLength;
+		
+		lengthPercentageText.text = (_percentage*100f)+"%";
+	}
+}
