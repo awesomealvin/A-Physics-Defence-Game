d6d1d7ab2de38c4110973e6616dff6c2f6857899
awesomealvin <fookinhellvin@outlook.com>
2018-03-09 23:31:54 +1300

    testing git bash

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour {
-
+	// test
 	public int points = 1000;
 	[SerializeField]
 	private GameManager gameManager;
